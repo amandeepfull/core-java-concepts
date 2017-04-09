76a6e6a772420c0820193aceb9f5b4bb6cf7b3ac
amandeepfull <amandeep.santokh@conversionsupport.com>
2017-04-09 15:37:55 +0530

    new

---
+++ multiple_main_method.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class abc
+{
+
+public static void main(Integer am[])
+{
+	System.out.println("tanushree");
+}
+
+public static void main(String arg[])
+{
+System.out.println("amandeep singh");
+}
+
+}
